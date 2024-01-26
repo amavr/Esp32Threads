4c7e386beb5842b7dba9a80b9ac522614bffaf1d
amavr <albert.mavrin@gmail.com>
2024-01-26 03:21:28 +0300

    add task

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,54 @@
 #include <Arduino.h>
 
 #include <GyverOLED.h>
+
+#define MOTION_PIN GPIO_NUM_12
+#define MOTION_CALM_DOWN 2000
+
 GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
 
-// #define MOTION_PIN GPIO_NUM_14;
-#define MOTION_PIN GPIO_NUM_14
+SemaphoreHandle_t sem;
+static portMUX_TYPE spin_lock = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR detectsMovement()
+{
+    BaseType_t woken = pdFALSE;
+    portENTER_CRITICAL_ISR(&spin_lock);
+    xSemaphoreGiveFromISR(sem, &woken);
+    portEXIT_CRITICAL_ISR(&spin_lock);
+    if (woken)
+    {
+        portYIELD_FROM_ISR(woken);
+    }
+}
+
+void taskMovement(void *pv)
+{
+    int last_state = 0;
+    while (true)
+    {
+        int motion_sensor = digitalRead(MOTION_PIN);
+        if (last_state != motion_sensor)
+        {
+            last_state = motion_sensor;
+            if (motion_sensor == HIGH)
+            {
+                digitalWrite(LED_BUILTIN, HIGH);
+
+                oled.home(); // курсор в 0,0
+                oled.print("MOVE!");
+            }
+            else
+            {
+                digitalWrite(LED_BUILTIN, LOW);
+
+                oled.home(); // курсор в 0,0
+                oled.print("Тихо!");
+            }
+        }
+        vTaskDelay(100);
+    }
+}
 
 void setup()
 {
@@ -13,39 +57,22 @@ void setup()
 
     pinMode(MOTION_PIN, INPUT_PULLUP);
 
-    oled.init();      // инициализация
-    oled.clear();     // очистка
-    oled.home();      // курсор в 0,0
+    // attachInterrupt(digitalPinToInterrupt(MOTION_PIN), detectsMovement, CHANGE);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    oled.init();  // инициализация
+    oled.clear(); // очистка
+    oled.home();  // курсор в 0,0
 
     oled.setScale(2); // масштаб текста (1..4)
 
-    oled.print("MOVE!");
-    // oled.setScale(3); // масштаб текста (1..4)
-    // oled.print("Привет!");
+    oled.print("INIT.");
 
-    // delay(1000);
-    // oled.setScale(1);
-    // // курсор на начало 3 строки
-    // oled.setCursor(0, 3);
-    // oled.print("GyverOLED v1.3.2");
-    // // курсор на (20, 50)
-    // oled.setCursorXY(20, 50);
-    // float pi = 3.14;
-    // oled.print("PI = ");
-    // oled.print(pi);
+    xTaskCreate(taskMovement, "Motion detection", 4096, NULL, 1, CONFIG_ARDUINO_RUNNING_CORE);
 }
 
 void loop()
 {
-    int motion_sensor = digitalRead(MOTION_PIN);
-    if(motion_sensor == HIGH)
-    {
-        oled.home();      // курсор в 0,0
-        oled.print("MOVE!");
-    }
-    else{
-        oled.home();      // курсор в 0,0
-        oled.print("Тихо!");
-    }
-    vTaskDelay(100);
 }
